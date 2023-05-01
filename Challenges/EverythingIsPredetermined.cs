bb179f5cfbfdee4e823d537379bd234641b9d66e
Brandon <brandon@brandonandzeus.com>
2023-04-30 21:44:29 -0700

    Add rest of files.

---
+++ Challenges/EverythingIsPredetermined.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.BuildersV2;
+using Trainworks.ConstantsV2;
+using Trainworks.ManagersV2;
+
+namespace BigOlBagOfMutators.Challenges
+{
+    public static class EverythingIsPredetermined
+    {
+        public static void MakeChallenge()
+        {
+            SpChallengeDataBuilder builder = new SpChallengeDataBuilder
+            {
+                ChallengeID = "Predetermined",
+                Name = "Everything is Predetermined",
+                Description = "Your destiny is predetermined! Pathing, Champions, and Card Upgrades already determined",
+                Mutators =
+                {
+                    CustomMutatorManager.GetMutatorDataByID("Mutator_ScriptedPage"),
+                    CustomMutatorManager.GetMutatorDataByID(VanillaMutatorIDs.OneTrackMind),
+                    CustomMutatorManager.GetMutatorDataByID("ErraticReflection"),
+                }
+            };
+
+            builder.BuildAndRegister();
+        }
+    }
+}
---
+++ CustomTriggers/OnOtherMonsterHit.cs
@@ -0,0 +1,80 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Trainworks.ConstantsV2;
+using Trainworks.Enums.MTTriggers;
+
+namespace BigOlBagOfMutators.CustomTriggers
+{
+    public static class OnOtherMonsterHit
+    {
+        public static CharacterTriggerData.Trigger MonsterTrigger = (new CharacterTrigger("Trigger_OnOtherMonsterHit")).GetEnum();
+        public static CharacterTriggerData.Trigger ImpTrigger = (new CharacterTrigger("Trigger_OnOtherImpHit")).GetEnum();
+    }
+
+    [HarmonyPatch(typeof(CharacterState), nameof(CharacterState.ApplyDamage))]
+    class OnOtherMonsterHitPatch
+    {
+        private struct CharacterHpState
+        {
+            public int hp;
+            public int armor;
+        }
+
+        private static CharacterHpState hpState = new CharacterHpState();
+        public static List<CharacterState> outCharacters = new List<CharacterState>();
+
+        public static void Prefix(CharacterState __instance)
+        {
+            hpState.hp = __instance.GetHP();
+            hpState.armor = __instance.GetStatusEffectStacks(VanillaStatusEffectIDs.Armor);
+        }
+
+        public static IEnumerator Postfix(IEnumerator enumerator, CharacterState __instance, CombatManager ___combatManager)
+        {
+            while (enumerator.MoveNext())
+                yield return enumerator.Current;
+
+            if (__instance == null)
+                yield break;
+
+            if (__instance.GetTeamType() == Team.Type.Heroes)
+                yield break;
+
+            var lastAttackerCharacter = __instance.GetLastAttackerCharacter();
+            if (lastAttackerCharacter == null || lastAttackerCharacter.IsDead || lastAttackerCharacter.IsDestroyed)
+                yield break;
+
+            // Test if the character was hit. If the HP or Armor changes then it should trigger OnHit
+            if (__instance.GetHP() == hpState.hp && __instance.GetStatusEffectStacks(VanillaStatusEffectIDs.Armor) == hpState.armor)
+            {
+                yield break;
+            }
+
+            outCharacters.Clear();
+            RoomState room = __instance.GetSpawnPoint().GetRoomOwner();
+            room.AddCharactersToList(outCharacters, Team.Type.Monsters);
+
+            foreach (CharacterState character in outCharacters)
+            {
+                if (character == __instance)
+                {
+                    continue;
+                }
+                yield return ___combatManager.QueueAndRunTrigger(character, OnOtherMonsterHit.MonsterTrigger, fireTriggersData: new CharacterState.FireTriggersData
+                {
+                    overrideTargetCharacter = __instance.GetLastAttackerCharacter(),
+                });
+                if (__instance.GetHasSubtype(SubtypeManager.GetSubtypeData(VanillaSubtypeIDs.Imp)))
+                {
+                    yield return ___combatManager.QueueAndRunTrigger(character, OnOtherMonsterHit.ImpTrigger, fireTriggersData: new CharacterState.FireTriggersData
+                    {
+                        overrideTargetCharacter = __instance.GetLastAttackerCharacter(),
+                    });
+                }
+            }
+        }
+    }
+}
